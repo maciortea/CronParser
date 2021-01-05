cad62563af6e367c9d4f6835f3b46c9add4132cc
Marian Ciortea <marian.ciortea@yahoo.com>
2021-01-05 20:55:07 +0000

    Cleanup unused statements.

--- src/CronParser/Context.cs
+++ src/CronParser/Context.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CronParser
+﻿namespace CronParser
 {
     public class Context
     {
         public string Input { get; }
-        public string Output { get; set; }
 
         public Context(string input)
         {
--- src/CronParser/Expressions/Operators/AsteriskExpression.cs
+++ src/CronParser/Expressions/Operators/AsteriskExpression.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CronParser.Expressions.Operators
+﻿namespace CronParser.Expressions.Operators
 {
     public class AsteriskExpression : Expression
     {
